db8951e28e54a3d9406793bec48e4a96c4b269c5
shubham <shubham.singh.bca-2023@asb.edu.in>
2025-02-25 16:02:14 +0530

    Add files via upload

---
+++ Pyramid_with_Prime_Numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n = 5;
+    int num = 2;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            while (!isPrime(num)) num++;
+            printf("%d", num++);
+        }
+        printf("\n");
+    }
+    return 0;
+}
